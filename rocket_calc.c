22711268e6c61aeee0a4478c4c77b84a2c903fb1
Andrew Trefry <139093712+SirTreefry@users.noreply.github.com>
2024-08-04 21:30:30 -0400

    Add files via upload

---
+++ rocket_calc.c
@@ -0,0 +1,17 @@
+//Andrew Trefry
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main(){
+float inlet_rad, inlet_ar;
+float throat_rad, throat_ar;
+float exit_rad, exit_rad;
+
+scanf("%f",inlet_rad);
+
+
+
+return 0;
+}
