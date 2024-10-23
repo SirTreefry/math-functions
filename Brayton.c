862bc131c9a718c6c55fb15f2344439a6a3518ac
Andrew Trefry <139093712+SirTreefry@users.noreply.github.com>
2024-10-23 17:25:07 -0400

    Add files via upload

--- Brayton.c
+++ Brayton.c
@@ -1,11 +1,14 @@
 //Andrew Trefry
 #include <stdio.h>
+#include <assert.h>
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+//void initgraph(int *graphicsDriver, int *graphicsMode, char *driverDirectoryPath);
 //cold air standard
 int main(){
 //preset values or measured values
+int i; //for gnu plot output loop
 float M_dot = .64; //mass flow of system
 float k = 1.4;
 float cp = 1.005;
@@ -34,7 +37,9 @@ float P_4;
 float P_5 = 101.25; //ambient air
 float thrust; //total thrust
 float in_v, out_v; //inlet and outlet velocity
-float Press_diff; //ratio between turbione exit and atmospheric pressure
+float Press_diff; //ratio between turbine exit and atmospheric pressure
+
+
 in_v = 0;
 P_2 = C_R * P_1; //pressure calculation for p2
 tm_2 = tm_1 * pow((P_2 / P_1), ((k-1)/k));//temp 2
@@ -46,10 +51,12 @@ W_C = M_dot * cp * (tm_1 - tm_2);//compressor work
 W_T = M_dot * cp * (tm_3 - tm_4);//turbine work
 BWR = (W_C * -1)/(W_T); //back work ratio
 N_eff = (W_T + W_C)/Q_in; //thermal effciency
-W_net = W_T - W_C;
+W_net = W_T - W_C;//net work done
 Press_diff = P_5 / P_4;
 out_v = sqrt(2* cp * tm_4 * N_eff * (1 - (1/pow(1/Press_diff,((k-1)/k)))));// equation for exit velocity
-thrust = M_dot * out_v;
+thrust = M_dot * out_v; //thrust equation
+
+
 printf("%f \n", P_2);
 printf("%f \n", tm_2);
 printf("%f \n", Q_in);
@@ -61,4 +68,11 @@ printf("Thermal Effciency %f \n", N_eff);
 printf("Power Produced %f \n", W_net);
 printf("Exit Velocity %f \n", out_v);
 printf("Thrust %f \n", thrust);
+
+FILE *gnuplot = popen("gnuplot", "w"); //writing to gnuplot
+fprintf(gnuplot, "plot '-'\n");
+for (i = 0; i < count; i++)
+    fprintf(gnuplot, "%g %g\n", x[i], y[i]);
+fprintf(gnuplot, "e\n");
+fflush(gnuplot);
 }
